7ef057ee1c62981dcce80390ab9cd78d791b9d7e
Matias <matiasonores@gmail.com>
2023-06-08 19:46:01 -0300

    desarrollo clase Promedio

--- PromedioProyecto/PromedioProyecto/Promedio.cs
+++ PromedioProyecto/PromedioProyecto/Promedio.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace PromedioProyecto
 {
-    class Promedio
-    {
+   public class Promedio
+   {
+      private int con = 0;
+      private double acum = 0;
+
+        public Promedio()
+        {
+        }
+
+      public void AgregarValor(int valor)
+      {
+         con++;
+         acum += valor;
+      }
+
+      public double CalcularPromedio()
+      {
+         if (con != 0) 
+         {
+            return acum / con;
+         }
+         else
+         {
+            return 0;
+         }
+      }
     }
 }
